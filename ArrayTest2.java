ef195415dfc928dd234cb3a6c5fff9326192bc25
ultimate717 <ultimate717@naver.com>
2024-02-16 19:14:04 +0900

    240216 scores array

--- ArrayTest2.java
+++ ArrayTest2.java
@@ -11,28 +11,26 @@ public class ArrayTest2 {
         System.out.println("다음 시험에서 1등 학생의 이름과 점수를 구하시오");
         System.out.println("시험에 응시한 학생수는 몇 명입니까?");
         int students = sc.nextInt();
-        int[] arr = new int[students];
-        String[] arr1 = new String[students];
+        int[] score = new int[students];
+        String[] stuname = new String[students];
 
-        for(int i = 0; i < arr.length; i++){
+        for(int i = 0; i < score.length; i++){
             System.out.println(i+1+"번째 학생 이름 " );
-            arr1[i] = sc.next();
+            stuname[i] = sc.next();
             System.out.println(i+1+"번째 학생 점수 ");
-            arr[i] = Integer.parseInt(sc.next());
+            score[i] = Integer.parseInt(sc.next());
         }
-        String name = "";
         int max = 0;
-        int stunum = 0;
-        for (int i = 0; i < students; i++){
-            if (arr[i] > max){
-                max =  arr[i];
-                stunum = i+1;
-                name = arr1[i];
+        String name = "";
+        for(int i = 0; i < stuname.length; i++){
+            if(score[i]>max){
+                max = score[i];
+                name = stuname[i];
+
             }
-            System.out.println("1등 점수는"+stunum+"번쨰"+name+"학생의"+max+"점이다");
         }
 
-
+        System.out.println("1등학생의 이름은"+name+"이고"+max+"이다");
 
 
 
