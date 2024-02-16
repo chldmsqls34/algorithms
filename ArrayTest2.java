c4b2c851b4145e8b57dca5a10b15f9e29cd7dda2
ultimate717 <ultimate717@naver.com>
2024-02-16 18:57:16 +0900

    2402216 scores array

--- ArrayTest2.java
+++ ArrayTest2.java
@@ -7,33 +7,29 @@ public class ArrayTest2 {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("다음 시험에서 1등 학생의 이름과 점수를 구하시오");
-
-
         System.out.println("시험에 응시한 학생수는 몇 명입니까?");
-        int numberOfStudent = sc.nextInt();
-        String[] names = new String[numberOfStudent];
-        int[] scores = new int[numberOfStudent];
+        int students = sc.nextInt();
+        int[] arr = new int[students];
+        String[] arr1 = new String[students];
 
-        System.out.println("응시한 학생의 이름을 입력하세요");
-        int nameOfStudent = sc.nextInt();
-        String[] name = new String[nameOfStudent];
-
-        System.out.println("응시한 학생의 점수를 입력하세요");
-        int scoreOfStudent = sc.nextInt();
-        String[] score = new String[scoreOfStudent];
-
-        
-        int max = 0;
-        for(int i = 0; i < scores.length; i++){
-            System.out.println("자연수 입력: " );
-            String num = sc.next();
-            scores[i] = Integer.parseInt(num);
-            if(max < scores[i]) {
-                max = scores[i];
-            }
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(i+1+"번째 학생 이름 " );
+            arr1[i] = sc.next();
+            System.out.println(i+1+"번째 학생 점수 ");
+            arr[i] = Integer.parseInt(sc.next());
         }
-            System.out.println("입력된 값 중 최대값은"+max+"이다");
+        String name = "";
+        int max = 0;
+        int stunum = 0;
+        for (int i = 0; i < students; i++){
+            if (arr[i] > max){
+                max =  arr[i];
+                stunum = i+1;
+                name = arr1[i];
+            }
+            System.out.println("1등 점수는"+stunum+"번쨰"+name+"학생의"+max+"점이다");
         }
 
 
