fb338560e2dac311933902b64eb799f00dd0bac9
chldmsqls34 <chldmsqls34@gmail.com>
2024-03-18 21:58:39 +0900

    practice

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        String a = sc.next();
+        int count =0;
+        for(int i=0;i<a.length();i++){
+            count++;
+        }
+        System.out.println(count);
     }
 }
