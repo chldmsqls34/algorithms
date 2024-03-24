c435cb07762f3694a92aa35983177b3642a701cc
chldmsqls34 <chldmsqls34@gmail.com>
2024-03-24 22:43:11 +0900

    practice

--- Main.java
+++ Main.java
@@ -2,14 +2,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(true){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            if(a>0&&b<10){
-                System.out.println(a+b);
-            }else{
-                break;
-            }
+        int sum= 0;
+        int n = sc.nextInt();
+        for(int i =1;i<n+1;i++){
+            sum += i;
         }
+        System.out.println(sum);
     }
 }
